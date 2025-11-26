81daf0bda94e1fac83328faa16caa062ad98c347
zhangping <675046166@qq.com>
2025-11-26 10:49:04 +0800

    license use ecdsa signature

--- pkg/license/license-key.go
+++ pkg/license/license-key.go
@@ -1,26 +1,22 @@
 package license
 
 import (
-	"crypto/rsa"
 	"github.com/golang-jwt/jwt/v4"
 )
 
 var (
-	PublicKey *rsa.PublicKey
+	PublicKey any
 
 	_publicKey string = `-----BEGIN PUBLIC KEY-----
-MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC4B5BfRrsXH2OXq/nILkMMYerU
-EoCcz/suR2GIPfSBU6dRDzdTrBQ4BbR5kojJrgKzlziLrqgLM8mlL1ukwc2roV5I
-wWbisJD0C5Jqw2LJj66Qs+0iUJsEe3lz/8FosnS28Vj4aIW7Mne2lZaMSygDosME
-oWS9wWRmC/BRtrg20QIDAQAB
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEb6XaaYKPxHcypR9JslSoF31eX/h3
+qWjZRsi5iM32W90R/t6LcvDs6T5SX9EfE/WdirlXsYSerI9jbXE/mx0dhg==
 -----END PUBLIC KEY-----`
 )
 
 func init() {
-	rsaPublicKey, err := jwt.ParseRSAPublicKeyFromPEM([]byte(_publicKey))
+	var err error
+	PublicKey, err = jwt.ParseECPublicKeyFromPEM([]byte(_publicKey))
 	if err != nil {
 		panic(err)
 	}
-	PublicKey = rsaPublicKey
-
 }
